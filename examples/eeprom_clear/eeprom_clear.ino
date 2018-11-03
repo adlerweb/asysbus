affb3351b31f624334c63eb75dce5d840194f594
Florian Knodt <git@adlerweb.info>
2018-11-03 16:21:06 +0100

    Update eeprom_clear.ino

--- examples/eeprom_clear/eeprom_clear.ino
+++ examples/eeprom_clear/eeprom_clear.ino
@@ -3,7 +3,7 @@
 void setup() {
   Serial.begin(115200);
   Serial.print("Erasing...");
-  for (int i = 0 ; i < EEPROM.length() ; i++) {
+  for (unsigned int i = 0 ; i < EEPROM.length() ; i++) {
     EEPROM.write(i, 0xFF);
   }
   Serial.print("Done.");
